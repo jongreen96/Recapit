b6c73eab123aa4544475930e641f03c6323f2112
Jon Green <jongreen1996@gmail.com>
2022-10-03 18:45:10 +0100

    Added PopularSubreddit component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import Header from './components/header/Header';
 import Sidebar from './components/sidebar/Sidebar';
+import PopularSubreddits from './components/popularSubreddits/PopularSubreddits';
 import './App.css';
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
 		<div className='App grid-container'>
       <Header />
       <Sidebar />
+      <PopularSubreddits />
 		</div>
 	);
 }
