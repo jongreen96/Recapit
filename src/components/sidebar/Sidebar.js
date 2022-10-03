7a9d366d0ee49e5cc89596aa103dbf98a22687b2
Jon Green <jongreen1996@gmail.com>
2022-10-03 19:29:28 +0100

    Made sidebar responsive

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -3,24 +3,28 @@ import './Sidebar.css';
 const Sidebar = () => {
 	return (
 		<div className='sidebar tile'>
+			<div className='no-wrap'>
 			<h2>
-				Current Subreddit: <br />
+				Current Subreddit: 
+			</h2>
+			<h2>
 				<strong>
 					r/<span className='blue'>Formula1</span>
 				</strong>
 			</h2>
-			<p>
+			</div>
+			<p className='description'>
 				The best independent Formula 1 community anywhere. News, stories
 				and discussion from and about the world of Formula 1.
 			</p>
             <div className='stats flex-container'>
                 <div>
-                    <h3 className='blue'><strong>6.6m</strong></h3>
+                    <h3 className='blue'><strong>6.9m</strong></h3>
                     <p>Members</p>
                 </div>
                 <div>
-                    <h3 className='blue'><strong>14.0k</strong></h3>
-                    <p>Online Now</p>
+                    <h3 className='blue'><strong>42.0k</strong></h3>
+                    <p>Online</p>
                 </div>
             </div>
 		</div>
