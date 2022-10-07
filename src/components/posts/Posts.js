403a703debaca5478ad4358eb2d4942213a457c0
Jon Green <jongreen1996@gmail.com>
2022-10-07 12:27:59 +0100

    Fixed bug stopping posts from loading

--- src/components/posts/Posts.js
+++ src/components/posts/Posts.js
@@ -1,15 +1,24 @@
 import './Posts.css';
 import Post from '../post/Post';
-import { useSelector } from 'react-redux';
-import { selectPosts } from '../../features/subreddit/subredditSlice';
+import { useSelector, useDispatch } from 'react-redux';
+import { selectPosts, fetchPosts, selectSubreddit } from '../../features/subreddit/subredditSlice';
+import { useEffect } from 'react';
 
 const Posts = () => {
+	const dispatch = useDispatch();
 	const posts = useSelector(selectPosts);
+	const subreddit = useSelector(selectSubreddit);
+
+	useEffect(() => {
+		dispatch(fetchPosts(subreddit));
+	}, [subreddit, dispatch]);
+
+	console.log(posts[6]);
 
 	return (
 		<div className='posts'>
-			{posts.map((post) => {
-				return <Post data={post} />;
+			{posts.map((post, i) => {
+				return <Post data={post} key={i} />;
 			})}
 		</div>
 	);
--- src/features/subreddit/subredditSlice.js
+++ src/features/subreddit/subredditSlice.js
@@ -1,44 +1,44 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 
 export const fetchPosts = createAsyncThunk(
-    'subreddit/fetchPosts',
-    async (subreddit) => {
-        const response = await fetch(
-            `https://www.reddit.com/r/${subreddit}/top/.json`
-        );
-        const data = await response.json();
-        return data.data.children.map((post) => post.data);
-    }
+	'subreddit/fetchPosts',
+	async (subreddit) => {
+		const response = await fetch(
+			`https://www.reddit.com/r/${subreddit}/top/.json`
+		);
+		const data = await response.json();
+		return data.data.children.map((post) => post.data);
+	}
 );
 
 const subredditSlice = createSlice({
-    name: 'subreddit',
-    initialState: {
-        subreddit: 'all',
-        posts: [],
+	name: 'subreddit',
+	initialState: {
+		subreddit: 'all',
+		posts: [],
         isLoading: false,
-        isError: false,
-    },
-    reducers: {
-        setSubreddit: (state, action) => {
-            state.subreddit = action.payload;
-        },
-    },
-    extraReducers: {
-        [fetchPosts.fulfilled]: (state, action) => {
-            state.posts = action.payload;
-            state.isLoading = false;
-            state.isError = false;
-        },
-        [fetchPosts.pending]: (state) => {
-            state.isLoading = true;
-            state.isError = false;
-        },
-        [fetchPosts.rejected]: (state) => {
-            state.isLoading = false;
-            state.isError = true;
-        }
-    },
+		isError: false,
+	},
+	reducers: {
+		setSubreddit: (state, action) => {
+			state.subreddit = action.payload;
+		},
+	},
+	extraReducers: {
+		[fetchPosts.fulfilled]: (state, action) => {
+			state.posts = action.payload;
+			state.isLoading = false;
+			state.isError = false;
+		},
+		[fetchPosts.pending]: (state) => {
+			state.isLoading = true;
+			state.isError = false;
+		},
+		[fetchPosts.rejected]: (state) => {
+			state.isLoading = false;
+			state.isError = true;
+		}
+	},
 });
 
 export const selectSubreddit = state => state.subreddit.subreddit;
