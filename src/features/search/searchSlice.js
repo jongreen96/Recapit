475ee0ac52df8217623324fac368a0a1df70336a
Jon Green <jongreen1996@gmail.com>
2022-10-08 11:26:56 +0100

    Added searchSlice & relevant api call to util

---
+++ src/features/search/searchSlice.js
@@ -0,0 +1,30 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const searchSlice = createSlice({
+	name: 'search',
+	initialState: {
+		query: '',
+		searchResults: [],
+	},
+	reducers: {
+		setQuery: (state, action) => {
+			state.query = action.payload;
+		},
+	},
+	extraReducers: {
+		[performSearch.fulfilled]: (state, action) => {
+			state.searchResults = action.payload.data;
+		},
+		[performSearch.pending]: (state) => {
+			state.searchResults = [];
+		},
+		[performSearch.rejected]: (state) => {
+			state.searchResults = [];
+		},
+	},
+});
+
+export const selectSearch = (state) => state.search;
+
+export const { setQuery } = searchSlice.actions;
+export default searchSlice.reducer;
--- src/store/Store.js
+++ src/store/Store.js
@@ -1,12 +1,14 @@
-import { configureStore } from "@reduxjs/toolkit";
-import subredditReducer from "../features/subreddit/subredditSlice";
-import postsReducer from "../features/posts/PostsSlice";
+import { configureStore } from '@reduxjs/toolkit';
+import subredditReducer from '../features/subreddit/subredditSlice';
+import postsReducer from '../features/posts/PostsSlice';
+import searchReducer from '../features/search/searchSlice';
 
 const store = configureStore({
-    reducer: {
-        subreddit: subredditReducer,
-        posts: postsReducer,
-    },
+	reducer: {
+		subreddit: subredditReducer,
+		posts: postsReducer,
+		search: searchReducer,
+	},
 });
 
 export default store;
