75fd74b5d2a0d0e621746e18a8e66eb35e1a9480
Jon Green <jongreen1996@gmail.com>
2022-10-09 12:36:38 +0100

    Clicking list item now takes you to subreddit

--- src/components/popularSubreddits/PopularSubreddits.js
+++ src/components/popularSubreddits/PopularSubreddits.js
@@ -1,28 +1,35 @@
+import { useDispatch } from 'react-redux';
+import { setSubreddit } from '../../features/subreddit/subredditSlice';
+
 import './PopularSubreddits.css';
 import logo from '../../images/recapit-logo.svg';
 
 const subreddits = [
-	'r/formula1',
-	'r/aww',
-	'r/askreddit',
-	'r/funny',
-	'r/gaming',
-	'r/movies',
-	'r/news',
-	'r/pics',
-	'r/science',
-	'r/sports',
+	'formula1',
+	'aww',
+	'askreddit',
+	'funny',
+	'gaming',
+	'movies',
+	'news',
+	'pics',
+	'science',
+	'sports',
 ];
 
 const PopularSubreddits = () => {
+	const dispatch = useDispatch();
+
 	return (
 		<div className='popular-subreddits tile'>
 			<h2>Popular Subreddits:</h2>
 			<ul>
 				{subreddits.map((subreddit, index) => (
-					<li key={index}>
+					<li key={index}
+					onClick={() => dispatch(setSubreddit(subreddit))}
+					>
 						<img src={logo} alt='subreddit icon' />
-						<h3>{subreddit}</h3>
+						<h3>r/{subreddit}</h3>
 					</li>
 				))}
 			</ul>
