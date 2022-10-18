c3f39e43a5e6ef4e01973098697a1fd12773b938
Jon Green <jongreen1996@gmail.com>
2022-10-18 17:16:53 +0100

    Added settings menu & set default to Darkmode

--- src/components/settings/Settings.js
+++ src/components/settings/Settings.js
@@ -1,20 +1,35 @@
 import settings from '../../images/settings-icon.svg';
-import { useDispatch } from 'react-redux';
+import toggleOff from '../../images/toggle-off-icon.svg';
+import toggleOn from '../../images/toggle-on-icon.svg';
+import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { setDarkmode } from '../../features/theme/ThemeSlice';
 
 const Settings = () => {
 	const dispatch = useDispatch();
+	const [active, setActive] = useState(false);
 	const handleClick = () => {
-		dispatch(setDarkmode());
+		setActive(!active);
 	};
+	const darkmode = useSelector((state) => state.theme.darkmode);
 
 	return (
-		<img
-			src={settings}
-			alt='Settings'
-			className='settings-icon'
-			onClick={handleClick}
-		/>
+		<>
+			<img
+				src={settings}
+				alt='Settings'
+				className='settings-icon settings'
+				onClick={handleClick}
+			/>
+			{active && (
+				<div className='settings-menu tile'>
+					<div onClick={() => dispatch(setDarkmode()) }>
+						<h4>Darkmode</h4>
+						<img src={darkmode ? toggleOn : toggleOff} alt='Toggle' />
+					</div>
+				</div>
+			)}
+		</>
 	);
 };
 
--- src/features/theme/ThemeSlice.js
+++ src/features/theme/ThemeSlice.js
@@ -3,7 +3,7 @@ import { createSlice } from "@reduxjs/toolkit";
 export const themeSlice = createSlice({
     name: "theme",
     initialState: {
-        darkmode: false,
+        darkmode: true,
     },
     reducers: {
         setDarkmode: (state) => {
